49c865c0b49ec3de0ddad79e4cb93f0f0c9d0358
lotfi-firdaouss <lotfi.firdaouss@gmail.com>
2021-07-30 11:19:17 +0100

    fixing the routes to register and adding routes and deleting unwanted component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { WelcomePageComponent } from './core/components/welcome-page/welcome-page.component';
+
 import { LoginPageComponent } from './core/components/login-page/login-page.component';
+import { RegisterPageComponent } from './core/components/register-page/register-page.component';
+
 import { ReportsListComponent } from './features/report/components/reports-list/reports-list.component';
+
+import { NoticeSliderComponent } from './shared/components/notice-slider/notice-slider.component';
+import { MenuComponent } from './shared/components/menu/menu.component';
+
 const routes: Routes = [
   { path : '' , component : WelcomePageComponent},
+  
   { path : 'login' , component : LoginPageComponent},
-  { path: 'list' , component: ReportsListComponent}
+  { path : 'register' , component : RegisterPageComponent},
+
+  { path: 'list' , component: ReportsListComponent},
+
+  { path: 'notice-slider' , component: NoticeSliderComponent},
+  { path: 'menu' , component: MenuComponent}
 ];
 
 @NgModule({
